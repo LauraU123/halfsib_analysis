e28b9ae4b2c1d5b62501c503a5483bab7842a68f
Laura Urbanska <laura.urbanska@unibe.ch>
2024-08-27 11:42:08 +0200

    added homozygosity rewrite file

---
+++ code/homozygosity.py
@@ -0,0 +1,21 @@
+# write homozygosity locations
+import pandas as pd
+import argparse
+
+def find_homozygosity(inputfile, outputfile):
+    df = pd.read_csv(inputfile, delim_whitespace=True)
+    with open(outputfile, "w") as f:
+        f.write("CHR;BP1;BP2")
+        for chr, start, end in zip(df["CHR"], df["POS1"], df["POS2"]):
+            f.write(f"{chr};{start/1000000};{end/1000000}\n") 
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(
+        description="Rewrite glycosylation file",
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter
+    )
+    parser.add_argument("--input_", required=True, help=".hom file")
+    parser.add_argument("--output", required=True, help=".csv file")
+    args = parser.parse_args()
+
+    find_homozygosity(args.input_, args.output)
