0fdf3c32ba0ecaca33e77a0f725bf42ade387280
Laura Urbanska <laura.urbanska@unibe.ch>
2024-09-18 10:31:48 +0200

    removed redundant chr list

--- code/halfsib_v2.py
+++ code/halfsib_v2.py
@@ -2,7 +2,6 @@ import pandas as pd
 import numpy as np
 import argparse
 
-#chromosomes = ["01","02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29" ]
 
 def map_file(filepath):
     """Output:list of  marker names from tab-delimited file."""
