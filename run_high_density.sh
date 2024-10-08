36bcaa4bcd2ace789c6fcfa86b77b78aa34afed0
Laura Urbanska <laura.urbanska@unibe.ch>
2024-10-08 12:35:42 +0200

    added cleanup step to high density

--- run_high_density.sh
+++ run_high_density.sh
@@ -1,4 +1,4 @@
 #! /bin/bash
 module load snakemake
 
-snakemake --cores all -F --configfile config/high_density.yaml -s run.smk --jobs 15 --cluster "sbatch -ppibu_el8  --mem={resources.mem} --cpus-per-task={threads} --time={resources.time}" --latency-wait 30
+snakemake --cores all cleanup -F --configfile config/high_density.yaml -s run.smk --jobs 15 --cluster "sbatch -ppibu_el8  --mem={resources.mem} --cpus-per-task={threads} --time={resources.time}" --latency-wait 30
