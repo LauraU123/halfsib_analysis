a3c51029b883c113a9625febdda768a9eda99b05
Laura Urbanska <laura.urbanska@unibe.ch>
2024-10-09 16:40:37 +0200

    test

--- run_medium_density.sh
+++ run_medium_density.sh
@@ -2,5 +2,4 @@
 
 module load snakemake
 
-snakemake --cores all cleanup --configfile config/medium_density.yaml -s run.smk --jobs 70 --latency-wait 30 --cluster "sbatch -ppibu_el8  --output=/dev/null --mem={resources.mem} --cpus-per-task={threads} --time={resources.time}"
-#snakemake --cores 1 cleanup --configfile config/medium_density.yaml -s run.smk
+snakemake --cores all -F  --configfile config/medium_density.yaml -s run.smk --jobs 70 --latency-wait 30 --cluster "sbatch -ppibu_el8  --output=/dev/null --mem={resources.mem} --cpus-per-task={threads} --time={resources.time}"
